7cc27153cddee7e9e0f9ad102fde499f67c9c74d
Bartlomiej_Loboz <lobozob@gmail.com>
2023-06-25 22:12:02 +0200

    devise2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   root 'home#index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
